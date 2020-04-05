65776396660e49b2bfdce79b996df12144c3739e
James Johnson <jtaylorjohnson@gmail.com>
2020-04-05 01:49:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,8 +15,9 @@ class School
   def add_student(name, grade)
     @name = name 
     @grade = grade
+    
+    unless roster[grade]
     roster[grade] = []
-    if 
     roster[grade] << name 
   end 
   
